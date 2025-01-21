777849271571c8fc2ba1cf965ca74105cc25c382
alex <alex@kknds.com>
2025-01-21 15:49:02 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,22 @@ function App() {
         <div className='flex justify-center items-center min-h-screen'>
             <div className='container flex flex-col justify-center items-center'>
                 <h1 className='text-5xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center'>
-                    Whisper WebGPU
+                    CDHU Whisper
                 </h1>
                 <h2 className='mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl'>
-                    ML-powered speech recognition directly in your browser
+                    Choose the file you would like to transcribe
                 </h2>
                 <AudioManager transcriber={transcriber} />
                 <Transcript transcribedData={transcriber.output} />
             </div>
 
             <div className='absolute bottom-4'>
-                Made with{" "}
+                Uppsala University - {" "}
                 <a
                     className='underline'
-                    href='https://github.com/xenova/transformers.js'
+                    href='https://www.uu.se/en/centre/digital-humanities-and-social-sciences'
                 >
-                    🤗 Transformers.js
+                    CDHU
                 </a>
             </div>
         </div>
